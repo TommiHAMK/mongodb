abf190b6cac13942283bd556e35460e24fcc4324
tommiHAMK <tommi.saksa@hamk.fi>
2025-04-12 17:07:41 +0300

    Routes Get all and one

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ require('dotenv').config();
 
 
 const dbURI ='mongodb+srv://'+process.env.DBUSERNAME+':'+process.env.DBPASSWORD+'@'+process.env.CLUSTER+'.mongodb.net/'+process.env.DB+'?retryWrites=true&w=majority&appName=Cluster0';
-
-console.log(dbURI);
+//console.log(dbURI);
+const app = express();
 
 mongoose.connect(dbURI)
 .then((result) =>
 {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log('Listening on port ' + PORT));
     console.log('Connected to DB');
 })
 .catch((err) => {
@@ -56,6 +58,24 @@ const getAll = async () => {
 }
 getAll();
 */
+
+app.get('/products', async (req,res) => {
+    try {
+        const result = await Product.find();
+        res.json(result);
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+
+app.get('/products/:id', async (req,res) => {
+    const id = req.params.id;
+        const product = await Product.findById(id);
+        res.json(product);
+});
+
+
 
 
 
