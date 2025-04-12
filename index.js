8ecd2bc63cd917bc18eadb32295ae6dbc82e7bbe
tommiHAMK <tommi.saksa@hamk.fi>
2025-04-12 15:53:50 +0300

    Database connection done

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const dbURI ='mongodb+srv://'+process.env.DBUSERNAME+':'+process.env.DBPASSWORD+'@'+process.env.CLUSTER+'.mongodb.net/'+process.env.DB+'?retryWrites=true&w=majority&appName=Cluster0';
+
+console.log(dbURI);
+
+mongoose.connect(dbURI)
+.then((result) =>
+{
+    console.log('Connected to DB');
+})
+.catch((err) => {
+    console.log(err);
+});
+
+ 
+
